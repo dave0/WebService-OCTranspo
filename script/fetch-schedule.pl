2515aedfc8b60b784e6d495f2c4b095080885d78
Dave O'Neill <dmo@dmo.ca>
2007-05-07 22:56:35 -0400

    Start converting to a packaged class

--- script/fetch-schedule.pl
+++ script/fetch-schedule.pl
@@ -1,92 +1,169 @@
 #!/usr/bin/perl -w
 
-use WWW::Mechanize;
-use HTML::Form::ForceValue;
-use HTML::TableExtract;
 use DateTime;
 use Data::Dumper;
-
-use constant DEBUG => 1;
-
 my $stop = shift or die q{stop number};
 my $route = shift or die q{route number};
 my $date = DateTime->now();
 
-my $mech = WWW::Mechanize->new(
-	cookie_jar => {}
-);
-$mech->agent_alias( 'Windows IE 6' );
+my $oc = WebService::OCTranspo::StopSchedules->new();
 
-# First, get the form page
-warn "Fetching start page for new session" if DEBUG;
-$mech->get('http://www.octranspo.com/tps/jnot/sptStartEN.oci');
+my $s  = $oc->schedule_for({
+	stop_id  => $stop,
+	route_id => $route,
+	date     => $date,
+});
 
-# Select the form
-warn "Selecting form via mech" if DEBUG;
-$mech->form_name('spt_date');
+print join("\n", @{ $s->{times} } );
+print "\n";
+print join("\n", @{ $s->{notes} } );
 
-warn "Forcing form values" if DEBUG;
-# Force some values.  Yes, all this duplication is necessary.
-warn $mech->current_form->dump;
-$mech->current_form()->force_value('theDate',     $date->ymd);
-$mech->current_form()->force_value('travelDate',  $date->ymd);
-$mech->current_form()->force_value('visibleDate', $date->month_name . ' ' . $date->day);
-$mech->current_form()->force_value('theTime',     '0000');
+package WebService::OCTranspo::StopSchedules;
+use WWW::Mechanize;
+use HTML::Form::ForceValue;
+use HTML::TableExtract;
+use Carp;
 
-warn "Submitting date form" if DEBUG;
-$mech->click();
+our $VERSION = '0.010';
 
-# Select a stop number
-warn "Selecting stop form" if DEBUG;
-$mech->form_name('spt_choose560');
-$mech->current_form->force_value('the560number', $stop);
-warn "Submitting stop form" if DEBUG;
-$mech->click();
+use constant DEBUG => 0;
 
-# Confirm the stop
-warn "Selecting stop confirm form" if DEBUG;
-$mech->form_name('spt_confirm560');
-warn "Submitting stop confirm form" if DEBUG;
-$mech->click();
 
-# ARGH! this works for one-route stops, but not for multi-route stops.
-# Need to parse the output and:
-# a) if it's asking for a route number, find the one we want and select
-# the appropriate checkbox
-# b) if it's not, parse the output for the stop data
-warn "Looking for $route";
-if( my ($checkname) = $mech->content =~ m{<label for="(check\d+)">$route}o ) {
-	warn "Got checkbox name $checkname";
-	$mech->form_name('spt_selectRoutes');
-	$mech->current_form()->force_value($checkname, 1);
-	$mech->click();
+sub new
+{
+	my ($class) = @_;
+	my $self = {};
+	$self->{mech} = WWW::Mechanize->new(
+		cookie_jar => {},
+		agent      => 'WebService-OCTranspo/' . $VERSION,
+	);
+
+	bless $self => $class;
+	return $self;
 }
 
-my $te = HTML::TableExtract->new( attribs => { class => 'spt_table' } );
-$te->parse( $mech->content );
+sub schedule_for
+{
+	my( $self, $args ) = @_;
 
-my @times;
-foreach $ts ( $te->tables ) {
-	print "Table (", join(',', $ts->coords), "):\n";
-	foreach $row ( $ts->rows ) {
-		foreach my $cell ( @$row ) {
-			next if ! defined $cell;
-			$cell =~ s/^\s+//s;
-			$cell =~ s/\s+$//s;
-			$cell =~ s/\s+/ /gs;
-			if( $cell =~ m/^\d+:\d+/ ) {
-				push @times, $cell;
-			}
+	foreach my $key ( qw( stop_id route_id date) ) {
+		if( ! exists $args->{$key} ) {
+			croak qq{$key argument required for schedule_for()};
 		}
+	}
+
+	$self->_reset();
+	$self->_select_date( $args->{date} );
+	$self->_select_stop( $args->{stop_id} );
+	$self->_select_route( $args->{route_id} );
+	
+	return $self->_parse_schedule();
+}
+
+sub _reset
+{
+	my ($self) = @_;
+	# First, get the form page
+	warn "Fetching start page for new session" if DEBUG;
+	$self->{mech}->get('http://www.octranspo.com/tps/jnot/sptStartEN.oci');
+}
+
+sub _select_date
+{
+	my ($self, $date) = @_;
+	# Select the form
+	warn "Selecting form via mech" if DEBUG;
+	$self->{mech}->form_name('spt_date');
+	warn $self->{mech}->current_form->dump if DEBUG;
+
+	my $form = $self->{mech}->current_form();
+
+	# Disable 'readonly' attribute
+	$form->find_input( 'travelDate' )->readonly(0);
+	$form->find_input( 'visibleDate' )->readonly(0);
+
+	warn "Forcing form values" if DEBUG;
+	# Force some values.  Yes, all this duplication is necessary.
+	$form->force_value('theDate',     $date->ymd);
+	$form->force_value('travelDate',  $date->ymd);
+	$form->force_value('visibleDate', $date->month_name . ' ' . $date->day);
+	$form->force_value('theTime',     '0000');
+
+	warn "Submitting date form" if DEBUG;
+	$self->{mech}->click();
+}
+
+sub _select_stop
+{
+	my ($self, $stop_id) = @_;
+	# Select a stop number
+	warn "Selecting stop form" if DEBUG;
+	$self->{mech}->form_name('spt_choose560');
+	warn $self->{mech}->current_form->dump if DEBUG;
+	$self->{mech}->current_form->force_value('the560number', $stop_id);
+	warn "Submitting stop form" if DEBUG;
+	$self->{mech}->click();
+
+	# Confirm the stop
+	warn "Selecting stop confirm form" if DEBUG;
+	$self->{mech}->form_name('spt_confirm560');
+	warn $self->{mech}->current_form->dump if DEBUG;
+	warn "Submitting stop confirm form" if DEBUG;
+	$self->{mech}->click();
+}
+
+sub _select_route
+{
+	my ($self, $route_id) = @_;
+	# By now we may have data for one-route stops, but not for
+	# multi-route stops.  
+	# Need to parse the output and:
+	# a) if it's asking for a route number, find the one we want and select
+	# the appropriate checkbox
+	# b) if it's not, parse the output for the stop data
+	warn "Looking for $route_id" if DEBUG;
+	if( my ($checkname) = $self->{mech}->content =~ m{<label for="(check\d+)">$route_id}o ) {
+		warn "Got checkbox name $checkname" if DEBUG;
+		$self->{mech}->form_name('spt_selectRoutes');
+		warn $self->{mech}->current_form->dump if DEBUG;
+		$self->{mech}->current_form()->force_value($checkname, 1);
+		$self->{mech}->click();
 	}
 }
 
-my @notes;
-$te = HTML::TableExtract->new( headers => [ 'Stop Note Information' ] );
-$te->parse( $mech->content );
+sub _parse_schedule
+{
+	my ($self) = @_;
 
-foreach my $row ($te->rows) {
-	push @notes, $row->[0];
+	my %schedule = (
+		'times' => [],
+		'notes' => [],
+	);
+
+	my $te = HTML::TableExtract->new( attribs => { class => 'spt_table' } );
+	$te->parse( $self->{mech}->content );
+
+	foreach $ts ( $te->tables ) {
+		print "Table (", join(',', $ts->coords), "):\n";
+		foreach $row ( $ts->rows ) {
+			foreach my $cell ( @$row ) {
+				next if ! defined $cell;
+				$cell =~ s/^\s+//s;
+				$cell =~ s/\s+$//s;
+				$cell =~ s/\s+/ /gs;
+				if( $cell =~ m/^\d+:\d+/ ) {
+					push @{$schedule{'times'}}, $cell;
+				}
+			}
+		}
+	}
+
+	$te = HTML::TableExtract->new( headers => [ 'Stop Note Information' ] );
+	$te->parse( $self->{mech}->content );
+
+	foreach my $row ($te->rows) {
+		push @{$schedule{'notes'}}, $row->[0];
+	}
+
+	return \%schedule;
 }
-
-print Dumper \@times, \@notes;
